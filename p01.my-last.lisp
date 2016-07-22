fc92c34917be4239d36d537c9099c5716946f62a
Zeyu Chen <xinyifly@live.cn>
2016-07-22 17:37:23 +0800

    feat(list): add my-last

---
+++ p01.my-last.lisp
@@ -0,0 +1,15 @@
+;;;; Find the last box of a list
+
+;;; Example:
+;;; * (my-last '(a b c d))
+;;; (D)
+
+(defun my-last (lista)
+  (if (null lista)
+      nil
+      (if (null (rest lista))
+          lista
+          (my-last (rest lista))
+          )
+      )
+  )
